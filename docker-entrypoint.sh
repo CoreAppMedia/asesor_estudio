08803caec5e53a17bbb084293c54eb0579af3f8f
CoreAppMedia <coreAppMedia@gmail.com>
2026-07-20 19:14:08 -0600

    despliegue Produccion v4

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 set -e
 
-# Cachear configuración, rutas y vistas para producción
+# 1. Ejecutar migraciones y seeders primero para asegurar que existan las tablas necesarias (como "cache" o "sessions")
+echo "Ejecutando migraciones de base de datos..."
+php artisan migrate --force
+
+echo "Ejecutando seeders de base de datos..."
+php artisan db:seed --force
+
+# 2. Cachear configuración, rutas y vistas para producción
 echo "Optimizando la caché de Laravel..."
 php artisan config:clear
 php artisan cache:clear
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
-
-# Ejecutar migraciones y seeders de forma segura en producción
-echo "Ejecutando migraciones de base de datos..."
-php artisan migrate --force
-
-echo "Ejecutando seeders de base de datos..."
-php artisan db:seed --force
 
 # Iniciar el proceso principal del contenedor (apache2-foreground)
 echo "Iniciando Apache..."
